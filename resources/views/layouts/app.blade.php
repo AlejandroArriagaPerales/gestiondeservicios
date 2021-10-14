3cb415729761d9960b9c530974134c6bb03d27b1
AlejandroA <1830491@upv.edu.mx>
2021-10-14 05:25:09 -0500

    Avance 14/10/2021 Corregido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
 <body>
     <div>
         
-        
+        <!--
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -81,7 +81,7 @@
                 </div>
             </div>
         </nav>
-        
+    -->
         
     
 
