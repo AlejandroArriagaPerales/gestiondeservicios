2c35a86ceacb342254ce822dcde7deb1a696dd6e
AlejandroA <1830491@upv.edu.mx>
2021-12-08 15:33:34 -0600

    RevisiónFinal 08/12/2021

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,8 @@ Route::get('/', HomeController::class);
 Route::post('dashboard', DashboardController::class)->name('dashboard') -> middleware('auth');
 Route::get('dashboard', DashboardController::class)->name('dashboard') -> middleware('auth');
 
-Route::get('/logout', LoginController::class);
-Route::post('/logout', LoginController::class);
+Route::get('/logout', 'Auth\LoginController@logout');
+Route::post('/logout', 'Auth\LoginController@logout');
 
 Route::post('registroprestadores', RegistroPrestadoresController::class)->name('registroprestadores') -> middleware('auth');
 Route::get('registroprestadores', RegistroPrestadoresController::class)->name('registroprestadores')-> middleware('auth');
