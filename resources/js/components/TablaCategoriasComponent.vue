a4d69e08dd44381ad2bf6ee1ef0fdcd2de305a56
AlejandroA <1830491@upv.edu.mx>
2021-12-08 16:26:58 -0600

    RevisiónFinal 08/12/2021

--- resources/js/components/TablaCategoriasComponent.vue
+++ resources/js/components/TablaCategoriasComponent.vue
@@ -94,7 +94,7 @@
                           ${{tab_categoria.costovisita}}
                         </td>
                         <td>
-                          <img src="{{tab_categoria.icono}}">
+                          <img src={{tab_categoria.nombre}}>
                         </td>
 
                         <td>
